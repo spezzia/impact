466c66255860a8125fb279ac72568505f26b4ec6
spezzia <spezzia.24.jl@gmail.com>
2019-11-06 15:02:52 -0600

    vida, cambio de personaje y disparos controlados

--- src/scenes/Bootloader.js
+++ src/scenes/Bootloader.js
@@ -10,7 +10,7 @@ class Bootloader extends Phaser.Scene{
     init()
     {
         console.log("Soy init");
-       direccion = true;
+        direccion = true;
         
     }
     preload()
@@ -19,32 +19,38 @@ class Bootloader extends Phaser.Scene{
         this.load.image("fondo","universo.jpg"); //  alias y archivo
         this.load.image("rose","rose1.png"); //  alias y archivo
         this.load.image("nombre",'nombre.png');
-        this.load.atlas('nave', 'nave_PP3/nave.png',
-        'nave_PP3/nave_atlas.json');
+        this.load.atlas('nave', 'nave_PP3/nave.png','nave_PP3/nave_atlas.json');
         this.load.animation('navee', 'nave_PP3/nave_anim.json');
         this.load.image('platano','platano.png');
         this.load.image('municion','MunicionNE1.png');
         this.load.image('sandia','sandia.png');
-        
-
-        this.load.atlas('enemigo1', 'enemigo1_PP3/enemigo1.png',
-        'enemigo1_PP3/enemigo1_atlas.json');
-        this.load.animation('enemigoo1', 'enemigo1_PP3/enemigo1_anim.json');
+        this.load.atlas('enemigo2', 'enemigo2_PP3/enemigo2.png','enemigo2_PP3/enemigo2_atlas.json');
+        this.load.animation('enemigoo1', 'enemigo2_PP3/enemigo2_anim.json');
+        this.load.image('vida','barra_vida/barra_vida00.png');
+        this.load.image('+vida','+vida.png');
+        this.load.image('corazon','corazonvida.png');
         
     }
 
     create()
     {
+        const keyCodes = Phaser.Input.Keyboard.KeyCodes;
         this.fondo = this.add.image(0,0,"fondo");
         this.fondo.setOrigin(0,0);
         this.fondo.setScale(.5);
+        this.fondo.setDepth(0);
         this.rose = this.add.image(1250,400,'rose').setScale(.7);
         this.nombre = this.add.image(625,10,'nombre').setScale(.7);
         this.nombre.setOrigin(.5,0);
-
-
-        this.enemigo1 = this.physics.add.sprite(800,200,'enemigo1').setScale(1);
-        this.enemigo1.anims.play('disparar');
+        this.nombre.setDepth(6);
+        this.vida = this.add.image(10,10,'vida').setOrigin(0).setScale(.5);
+        this.corazon = this.add.image(6,110,'corazon').setOrigin(0).setScale(1.7);
+        this.masvida = this.physics.add.sprite(625,0,'+vida').setScale(.7);
+        this.masvida.body.setGravityY(30);
+        this.masvida.body.setGravityX(10);
+       
+        this.enemigo1 = this.physics.add.sprite(800,200,'enemigo2').setScale(.35).setFlipX(1);
+        this.enemigo1.anims.play('disparare2');
         this.enemigo1.on('animationcomplete',()=>{
             this.municion = this.add.image(this.enemigo1.x-60,this.enemigo1.y,'municion').setScale(.1); 
             this.timeline = this.tweens.createTimeline();
@@ -54,12 +60,12 @@ class Bootloader extends Phaser.Scene{
                 duration: 5000,
             });
             this.timeline.play();
-            this.enemigo1.anims.play('disparar');
+            this.enemigo1.anims.play('disparare2');
             console.log("Hola");
             
         },this);
 
-        this.nave = this.physics.add.sprite(100, 200, 'nave').setScale(1.2);
+        this.nave = this.physics.add.sprite(100, 200, 'nave').setScale(1.4);
         this.nave.setBounce(0.1);
         this.nave.setCollideWorldBounds(true);
         this.flechas = this.input.keyboard.createCursorKeys();
@@ -92,27 +98,15 @@ class Bootloader extends Phaser.Scene{
         this.nave.anims.play('carga');
         this.sandia = this.physics.add.group();
 
-        
-
-
-       /* var timer1 = this.time.addEvent({
-            delay: 250,                // ms
-            callback: () =>  {
-                
-
-            },
-           loop:true
-        });*/
         this.palatano = this.physics.add.group();
         console.log(this.palatano);
-        
-
-        var timer = this.time.addEvent({
-            delay: 1000,                // ms
+        this.timer = this.time;
+        this.timer.addEvent({
+            delay: 500,                // ms
             callback: () =>  {
                 var ano  = this.palatano.create(this.nave.x+60,this.nave.y+5,'platano').setScale(.9); 
                 ano.setBounce(0.2);
-                ano.setCollideWorldBounds(true);
+                //ano.setCollideWorldBounds(true);
                 this.timeline = this.tweens.createTimeline();
                 this.timeline.add({
                     targets: [ano],
@@ -126,7 +120,18 @@ class Bootloader extends Phaser.Scene{
             },
            loop:true
         });
+        this.timer.paused = true;
+        this.saltar = this.input.keyboard.addKey(keyCodes.SHIFT);
+        this.saltar.on('down',()=>
+        {
+           this.timer.paused = false;
+        })
 
+        this.saltar.on('up',()=>
+        {
+            this.timer.paused = true;
+        })
+        
         this.physics.add.collider(this.palatano, this.enemigo1,function(enemigo,platano){
             //console.log(enemigo);
             platano.destroy();
@@ -140,7 +145,10 @@ class Bootloader extends Phaser.Scene{
             this.nave.x -= 4; 
         }  
         if( this.flechas.right.isDown ){
-            this.nave.x += 4; 
+            if(this.nave.x + 4 <= 625 )
+            {
+                this.nave.x += 4;
+            } 
         }   
         if( this.flechas.down.isDown ){
             this.nave.y += 4; 
